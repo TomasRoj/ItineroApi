8770d5f1ab067b304bdbcda938b9df3e434285bf
Tomáš Roj <rojtomas@email.cz>
2025-04-16 10:04:14 +0200

    Small fix of attraction model

--- ItineroApi/Models/Attraction.cs
+++ ItineroApi/Models/Attraction.cs
@@ -7,20 +7,20 @@ namespace ItineroApi.Models
     {
         public int Id { get; set; }
         public string Country { get; set; }
-        public int? CityId { get; set; }
+        public int? city_id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public string Address { get; set; }
         public decimal? Latitude { get; set; }
         public decimal? Longitude { get; set; }
-        public int? EstimatedVisitTime { get; set; }
-        public decimal? EntranceFee { get; set; }
-        public string CurrencyCode { get; set; }
-        public string OpeningHours { get; set; }
+        public int? estimated_visit_time { get; set; }
+        public decimal? Entrance_fee { get; set; }
+        public string Currency_code { get; set; }
+        public string Opening_hours { get; set; }
         public string Website { get; set; }
-        public string PhotoUrl { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public string Photo_url { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
     }
 }
